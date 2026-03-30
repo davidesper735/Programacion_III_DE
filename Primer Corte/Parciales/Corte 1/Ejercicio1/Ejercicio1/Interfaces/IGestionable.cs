4948a1695b1ee393a3f607e5fb622f664bb4026c
David Esper <david.esper735@gmail.com>
2026-03-30 09:21:04 -0500

    Cambios

---
+++ Primer Corte/Parciales/Corte 1/Ejercicio1/Ejercicio1/Interfaces/IGestionable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Interfaces
+{
+    public interface IGestionable
+    {
+        void Crear();
+        void Leer();
+        void Actualizar();
+        void Eliminar();
+    }
+}
---
+++ Primer Corte/Parciales/Corte 1/Ejercicio1/Ejercicio1/Metodos/Repuesto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio1.Interfaces;
+
+namespace Ejercicio1.Metodos
+{
+    public class Repuesto : IGestionable
+    {
+        public string Id { set; get; }
+
+        public string Nombre { set; get; }
+
+        public decimal Precio { set; get;  }
+
+        public void Actualizar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Crear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Eliminar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Leer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
